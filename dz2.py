6600af64cca9c48257db8f2153ebb265ce51e506
CTu4uK <46240190+CTu4uK@users.noreply.github.com>
2021-12-12 15:16:15 +0300

    Add files via upload

---
+++ dz2.py
@@ -0,0 +1,35 @@
+# 2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
+# Значения данных атрибутов должны передаваться при создании экземпляра класса. Атрибуты сделать защищенными.
+# Определить метод расчета массы асфальта, необходимого для покрытия всего дорожного полотна. Использовать формулу:
+# длина*ширина*масса асфальта для покрытия одного кв метра дороги асфальтом, толщиной в 1 см*число см толщины полотна.
+# Проверить работу метода.
+# Например: 20м*5000м*25кг*5см = 12500 т
+
+class Road:
+
+    # удельная масса 1кв.м. дорожного полотна толщиной 1 см (тонн)
+    _surface_spec_gravity: float = 0.02
+
+    def __init__(self, length: [int, float], width: [int, float]):
+
+        self._length = float(length)
+        self._width = float(width)
+
+    def get_surface_gravity(self, thickness: float) -> [float, None]:
+        try:
+            return (self._length * self._width
+                    * thickness * self._surface_spec_gravity)
+        except TypeError:
+            return None
+
+
+if __name__ == "__main__":
+    try:
+        road = Road(5000, 10)
+        print(
+            "Масса дорожного покрытия составит:",
+            road.get_surface_gravity(20),
+            "тонн"
+        )
+    except TypeError:
+        print("класс Road требует 2 позиционных аргумента")
